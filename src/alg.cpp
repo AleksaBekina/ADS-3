df099c864ea016fbefa6fd36ce8ea68122de849c
AleksaBekina <116704236+AleksaBekina@users.noreply.github.com>
2023-03-18 22:14:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,23 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int left = 0;
+  int right = size - 1;
+  int count = 0;
+  while (left <= right) {
+    int middle = (left + right) / 2;
+    if (arr[middle] == value) {
+      count++;
+      while(arr[--middle] == value) count++;
+      middle += count;
+      while (arr[++middle] == value) count++;
+      return count;
+    }
+    else if (arr[middle] < value) {
+      left = middle + 1;
+    } else {
+            right = middle - 1;
+    }
+  }
   return 0; // если ничего не найдено
 }
